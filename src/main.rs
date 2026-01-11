b026418b67b36b9e4819c925c5cd89b6afbe51a2
Vinayak Goyal <vinayakankugoyal@gmail.com>
2026-01-11 00:03:45 -0500

    return a mostly dummy header section

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,17 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                let response = [
+                    0x04,0xD2, // Packet ID
+                    0x80, // QR=1,OPCODE=0000,AA=0,TC=0,RD=0
+                    0x00, // RA=0,Z=000,RCODE=0000 
+                    0x00,0x00, //QDCOUNT=0000000000000000
+                    0x00,0x00, //ANCOUNT=0000000000000000
+                    0x00,0x00, //NSCOUNT=0000000000000000
+                    0x00,0x00, //ARCOUNT=0000000000000000
+
+
+                ];
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
