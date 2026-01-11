ac6f61218d01eba3e050190187a9149017a1da8e
Vinayak Goyal <vinayakankugoyal@gmail.com>
2026-01-11 00:22:25 -0500

    return a dummy question

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,25 @@ fn main() {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
                 let response = [
+                    // Header
                     0x04,0xD2, // Packet ID
                     0x80, // QR=1,OPCODE=0000,AA=0,TC=0,RD=0
                     0x00, // RA=0,Z=000,RCODE=0000 
-                    0x00,0x00, //QDCOUNT=0000000000000000
+                    0x00,0x01, //QDCOUNT=0000000000000001
                     0x00,0x00, //ANCOUNT=0000000000000000
                     0x00,0x00, //NSCOUNT=0000000000000000
                     0x00,0x00, //ARCOUNT=0000000000000000
-
-
+                    // Question
+                    // Domain Name
+                    0x0c, //codecrafters
+                    b'c',b'o',b'd',b'e',b'c',b'r',b'a',b'f',b't',b'e',b'r',b's',
+                    0x02, //io
+                    b'i',b'o',
+                    0x00,
+                    // Type
+                    0x00,0x01,
+                    // Class
+                    0x00,0x01,
                 ];
                 udp_socket
                     .send_to(&response, source)
